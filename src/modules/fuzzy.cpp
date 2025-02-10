26158010f0d2fdce881e71ef3e72f5f2211298c4
BrainStone <yannick@tedworld.de>
2025-02-11 00:13:33 +0100

    Implemented single threaded password variant checking

--- src/modules/fuzzy.cpp
+++ src/modules/fuzzy.cpp
@@ -3,6 +3,7 @@
 #include <security/pam_modules.h>
 #include <shadow.h>
 
+#include <generator>
 #include <iostream>
 
 #include "pam_exception.hpp"
@@ -10,11 +11,74 @@
 
 using namespace std::string_literals;
 
+constexpr std::string_view ALLOWED_CHARS =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{};:'\",.<>?/\\|`~";
+
 bool check_password(std::string_view password, std::string_view stored_hash) {
 	thread_local crypt_data data{};
 
 	char* computed_hash = crypt_r(password.data(), stored_hash.data(), &data);
 	return computed_hash != nullptr && computed_hash == stored_hash;
+}
+
+std::generator<std::string> levenshtein_variants(std::string_view password) {
+	// Storage. Attempt to avoid changing buffer size as much as possible
+	std::string variant;
+	variant.reserve(password.size() + 1);
+
+	// Deletions
+	{
+		variant.resize(password.size() - 1);
+		std::copy(password.begin() + 1, password.end(), variant.begin());
+
+		co_yield variant;
+
+		auto p_it = password.begin();
+		const auto v_end = variant.end();
+		for (auto v_it = variant.begin(); v_it != v_end; ++p_it, ++v_it) {
+			*v_it = *p_it;
+
+			co_yield variant;
+		}
+	}
+
+	// Substitutions
+	{
+		variant = password;
+
+		const auto v_end = variant.end();
+		for (auto v_it = variant.begin(); v_it != v_end; ++v_it) {
+			char original = *v_it;
+
+			for (char c : ALLOWED_CHARS) {
+				if (original != c) {
+					*v_it = c;
+
+					co_yield variant;
+				}
+			}
+
+			*v_it = original;
+		}
+	}
+
+	// Insertions
+	{
+		variant.resize(password.size() + 1);
+
+		const auto v_end = variant.rend();
+		auto v_it = variant.rbegin();
+		while (true) {
+			for (char c : ALLOWED_CHARS) {
+				*v_it = c;
+
+				co_yield variant;
+			}
+
+			if (++v_it == v_end) break;
+			*(v_it - 1) = *v_it;
+		}
+	}
 }
 
 // PAM authentication function
@@ -37,11 +101,13 @@ PAM_EXPORT int pam_sm_authenticate(pam_handle_t* pamh, [[maybe_unused]] int flag
 		}
 		std::string_view stored_hash = shadow_entry->sp_pwdp;
 
-		if (check_password(password, stored_hash)) {
-			return PAM_SUCCESS;
+		if (check_password(password, stored_hash)) return PAM_SUCCESS;
+
+		for (const auto& password_variant : levenshtein_variants(password)) {
+			if (check_password(password_variant, stored_hash)) return PAM_SUCCESS;
 		}
 
-		return PAM_IGNORE;
+		return PAM_AUTH_ERR;
 	});
 }
 
